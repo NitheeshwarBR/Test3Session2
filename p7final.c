09999d2e2922342ab6141ee4d3f44ff3ddf1163f
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2022-03-03 08:49:33 +0000

    p7.c

--- p7final.c
+++ p7final.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction Fraction;
+int find_gcd(int a,int b)
+{
+   int d;
+  while(b!=0)
+    {
+      a=b;
+      d=a%b;
+      b=d;
+    }
+  return a;
+}
+int main()
+{
+  printf("%d\n",find_gcd(16,8));
+}
